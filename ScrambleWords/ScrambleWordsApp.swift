6ff5b0e42733dc5980d25d21d8487bfc152bb080
Ahmad Kaddoura <kaddoura.ahmad@gmail.com>
2024-01-15 19:45:21 -0800

    Initial Commit

---
+++ ScrambleWords/ScrambleWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambleWordsApp.swift
+//  ScrambleWords
+//
+//  Created by ahmad kaddoura on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrambleWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
